d71cd747bb2e90f89701728e40bbba24050491cd
fmalinowski <francois@francois-malinowski.fr>
2015-02-22 15:28:50 -0800

    fm - Add basic stuff to connect to twitter with twitter4j

--- apps/Streaming-Twitter/src/edu/ucsb/cs290d/tweet_retriever/Main.java
+++ apps/Streaming-Twitter/src/edu/ucsb/cs290d/tweet_retriever/Main.java
@@ -1,9 +1,22 @@
 package edu.ucsb.cs290d.tweet_retriever;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setOAuthConsumerKey(TwitterCredentials.CONSUMER_KEY)
+		  .setOAuthConsumerSecret(TwitterCredentials.CONSUMER_SECRET)
+		  .setOAuthAccessToken(TwitterCredentials.ACCESS_TOKEN)
+		  .setOAuthAccessTokenSecret(TwitterCredentials.ACCESS_TOKEN_SECRET);
+		
+		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+		
+		// PLACE YOUR CODE TO USE TWITTER4J
 	}
 
 }
---
+++ apps/Streaming-Twitter/src/edu/ucsb/cs290d/tweet_retriever/TwitterCredentials.java
@@ -0,0 +1,8 @@
+package edu.ucsb.cs290d.tweet_retriever;
+
+public class TwitterCredentials {
+	public static final String CONSUMER_KEY = "";
+	public static final String CONSUMER_SECRET = "";
+	public static final String ACCESS_TOKEN = "";
+	public static final String ACCESS_TOKEN_SECRET = "";
+}
