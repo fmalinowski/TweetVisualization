91182bd4cd008519e403c514747ae30d2155b420
Wei Dai <wdai@novacoast.com>
2015-02-22 15:25:54 -0800

    RawTwitterStream Test

--- apps/TweetVisualization/src/tweetvisualization/TweetVisualization.java
+++ apps/TweetVisualization/src/tweetvisualization/TweetVisualization.java
@@ -1,9 +1,12 @@
 package tweetvisualization;
 
+import twitter4j.*;
+
 public class TweetVisualization {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         // TODO code application logic here
+        
     }
     
 }
