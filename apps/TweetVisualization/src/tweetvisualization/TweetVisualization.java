81cfdb5a6894a3ec714cfe7ab5b45517e9c195f8
Wei Dai <wdai@novacoast.com>
2015-02-22 15:29:16 -0800

    Merge to master

--- apps/TweetVisualization/src/tweetvisualization/TweetVisualization.java
+++ apps/TweetVisualization/src/tweetvisualization/TweetVisualization.java
@@ -6,7 +6,19 @@ public class TweetVisualization {
 
     public static void main(String[] args) throws TwitterException {
         // TODO code application logic here
-        
-    }
-    
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        RawStreamListener listener = new RawStreamListener() {
+            @Override
+            public void onMessage(String rawJSON) {
+                System.out.println(rawJSON);
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+        twitterStream.addListener(listener);
+        twitterStream.sample();
+    }       
 }
