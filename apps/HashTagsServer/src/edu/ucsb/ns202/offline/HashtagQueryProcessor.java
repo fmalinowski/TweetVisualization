7d94a404fb7ca536a58656323ab5832a771b9d43
fmalinowski <francois@francois-malinowski.fr>
2015-02-26 23:03:36 -0800

    fm - Add HashtagGraph to build the graph of hashtags and to generate the JSON object that will be needed to make the visualization of the graph with D3

---
+++ apps/HashTagsServer/src/edu/ucsb/ns202/offline/HashtagQueryProcessor.java
@@ -0,0 +1,28 @@
+package edu.ucsb.ns202.offline;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.ucsb.ns202.HashtagGraph;
+
+public class HashtagQueryProcessor {
+	
+	private String hashtag;
+	
+	public HashtagQueryProcessor(String hashtag) {
+		this.hashtag = hashtag;
+	}
+
+	public JSONObject query() throws JSONException {
+		
+		HashtagGraph hashtagGraph = new HashtagGraph();
+		hashtagGraph.addEgde("#Node1", "#nODe2");
+		hashtagGraph.addEgde("#Node1", "#nODe3");
+		hashtagGraph.addEgde("#Node4", "NODE5");
+		hashtagGraph.addEgde("#Node4", "NODE6");
+		hashtagGraph.addEgde("#Node4", "NODE7");
+		hashtagGraph.addEgde("#Node4", "NODE1");
+		
+		return hashtagGraph.getNodesAndEdgesAsJSON();
+	}
+
+}
--- apps/HashTagsServer/src/edu/ucsb/ns202/offline/ServerRunner.java
+++ apps/HashTagsServer/src/edu/ucsb/ns202/offline/ServerRunner.java
@@ -1,10 +1,107 @@
 package edu.ucsb.ns202.offline;
 
-import edu.ucsb.ns202.IServerRunner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.Map;
 
-public class ServerRunner implements IServerRunner {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import edu.ucsb.ns202.IServerRunner;
+import edu.ucsb.ns202.ParamUtilities;
+
+public class ServerRunner implements IServerRunner{
 	
+	private static HashtagQueryProcessor hashtagQueryProcessor;
+
 	public void run() {
-		//TODO
+		HttpServer server;
+		try {
+			server = HttpServer.create(new InetSocketAddress(8000), 0);
+			server.createContext("/", new StaticContentHandler());
+		    server.createContext("/request", new HashTagRequestHandler());
+		    server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
+		    server.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
+	
+	class HashTagRequestHandler implements HttpHandler {
+	    public void handle(HttpExchange t) throws IOException {
+	    	String query = t.getRequestURI().getQuery();
+	    	// Will get a map with as a key the parameter and as a value the value
+	    	// This serves the requests like ?param1=value1&param2=value2 ... 
+	    	Map<String, String> params = ParamUtilities.queryToMap(query);
+	    	
+	    	hashtagQueryProcessor = new HashtagQueryProcessor(params.get("hashtag"));
+	    	JSONObject jsonResponse = null;
+	    	
+			try {
+				jsonResponse = hashtagQueryProcessor.query();
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			
+	    	String strJsonResponse = jsonResponse.toString();
+	    	
+	    	Headers h = t.getResponseHeaders();
+	    	h.add("Content-Type", "application/json");
+			t.sendResponseHeaders(200, strJsonResponse.length());
+	    	
+	    	OutputStream os = t.getResponseBody();
+	    	
+	    	os.write(strJsonResponse.getBytes());
+			os.close();
+	    }
+	 }
+	
+	class StaticContentHandler implements HttpHandler {
+		public void handle(HttpExchange t) throws IOException {
+			URI uri = t.getRequestURI();
+			String path = uri.getPath();
+			
+			if (path.equals("/")) {
+				
+				InputStream htmlFileInputStream = ServerRunner.class.getClassLoader().getResourceAsStream("resources/index.html");
+					
+				t.sendResponseHeaders(200, 0);
+				OutputStream os = t.getResponseBody();
+				final byte[] buffer = new byte[0x10000];
+				int count = 0;
+				while ((count = htmlFileInputStream.read(buffer)) >= 0) {
+					os.write(buffer,0,count);
+				}
+				os.close();
+				htmlFileInputStream.close();
+			}
+			else if (path.equals("/visualization.js")) {
+				
+				InputStream htmlFileInputStream = ServerRunner.class.getClassLoader().getResourceAsStream("resources/visualization.js");
+				
+				Headers h = t.getResponseHeaders();
+				h.add("Content-Type", "application/javascript");
+				
+				t.sendResponseHeaders(200, 0);
+				OutputStream os = t.getResponseBody();
+				final byte[] buffer = new byte[0x10000];
+				int count = 0;
+				while ((count = htmlFileInputStream.read(buffer)) >= 0) {
+					os.write(buffer,0,count);
+				}
+				os.close();
+				htmlFileInputStream.close();
+			}
+		}
+	}
+
 }
