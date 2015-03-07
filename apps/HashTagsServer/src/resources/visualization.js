b7293a184280e3125ac2aa5b390e4a1bb6477642
fmalinowski <francois@francois-malinowski.fr>
2015-03-07 12:42:41 -0800

    fm - Clean a little bit the JS code cause it really looked like crap man! Still looks like crap but it's better

--- apps/HashTagsServer/src/resources/visualization.js
+++ apps/HashTagsServer/src/resources/visualization.js
@@ -2,7 +2,7 @@ var svg, graphFrame;
 
 function displayGraph(JSONdata) {
 	var resultContainerSel;
-	var width, height, forceGraph, link, nodesGroup, node, text, zoom;
+	var width, height, forceGraph, zoom, link, nodesGroup;
 
 	width = 800;
 	height = 500;
@@ -35,33 +35,9 @@ function displayGraph(JSONdata) {
 				.links(JSONdata.links)
 				.start();
 
-	link = graphFrame
-				.selectAll(".link")
-				.data(JSONdata.links, function(d) { return d.source.id + "-" + d.target.id; })
-				.enter().append("line")
-				.classed("link", true);
-				// We could have used .style("stroke-width", function(d) { return Math.sqrt(d.value);}); //value of 5 max!
-				// to have a stroker link according to the value data attribute
+	link = createEdges(JSONdata, graphFrame);
 
-	nodesGroup = graphFrame.selectAll("g")
-					.data(JSONdata.nodes, function(d) { return d.id; })
-					.enter()
-					.append("g");
-
-	node = nodesGroup.append("circle")
-				.classed("node", true)
-				.attr("r", 5) // We could make a function to get the radius bigger: attr(r, function(d) { return d.value; })
-				.on("mouseover", mouseoverNode)
-				.on("mouseout", mouseoutNode)
-				.call(forceGraph.drag);
-				// We could have used .style("fill", function(d) { return color(d.group); }) 
-				// to color in different colros using the data attribute group.
-
-	text = nodesGroup.append("text")
-					.text(function(d) { return d.name; })
-					.classed("node-title", true)
-					.attr("x", 8)
-    				.attr("y", ".31em");
+	nodesGroup = createNodes(JSONdata, graphFrame, forceGraph);
 
 	forceGraph.on("tick", function() {
 		link.attr("x1", function(d) { return d.source.x; })
@@ -74,6 +50,44 @@ function displayGraph(JSONdata) {
 
 }
 
+function createEdges(JSONdata, graphFrame) {
+	return graphFrame
+		.selectAll(".link")
+		.data(JSONdata.links, function(d) { return d.source.id + "-" + d.target.id; })
+		.enter().append("line")
+		.classed("link", true);
+		// We could have used .style("stroke-width", function(d) { return Math.sqrt(d.value);}); //value of 5 max!
+		// to have a stroker link according to the value data attribute
+}
+
+function createNodes(JSONdata, graphFrame, forceGraph) {
+	var nodesGroup;
+
+	nodesGroup = graphFrame.selectAll("g")
+					.data(JSONdata.nodes, function(d) { return d.id; })
+					.enter()
+					.append("g");
+
+	// Append the circles
+	nodesGroup.append("circle")
+		.classed("node", true)
+		.attr("r", 5) // We could make a function to get the radius bigger: attr(r, function(d) { return d.value; })
+		.on("mouseover", mouseoverNode)
+		.on("mouseout", mouseoutNode)
+		.call(forceGraph.drag);
+		// We could have used .style("fill", function(d) { return color(d.group); }) 
+		// to color in different colros using the data attribute group.
+
+	// Append the labels
+	nodesGroup.append("text")
+		.text(function(d) { return d.name; })
+		.classed("node-title", true)
+		.attr("x", 8)
+    	.attr("y", ".31em");
+
+    return  nodesGroup;
+}
+
 function rescale() {
 	var translation, scale;
 	
@@ -84,19 +98,45 @@ function rescale() {
 }
 
 function mouseoverNode(nodeData) {
-	var node, nodeText, groupNode, targetNodes;
+	var targetNodes;
 
-	node = d3.select(this);
-	nodeText = d3.select(this.parentNode).select("text");
-	
-	$(".results__info").html("Over: " + nodeData.name);
-	node.classed("node--hovered", true);
-	nodeText.classed("node-title--node-hovered", true);
+	highlightHoveredNode(true, nodeData, this);
+	putNodeInForeground(this);
+	targetNodes = highlightEdgesAndGetTargetNodes(true, nodeData);
+	highlighTargetNodes(true, targetNodes);
+}
 
-	groupNode = this.parentNode;
+function mouseoutNode(nodeData) {
+	var targetNodes;
+
+	highlightHoveredNode(false, nodeData, this);
+	putNodeInForeground(this);
+	targetNodes = highlightEdgesAndGetTargetNodes(false, nodeData);
+	highlighTargetNodes(false, targetNodes);
+}
+
+function highlightHoveredNode(highlightBool, nodeData, node) {
+	var displayedText, nodeEl, nodeText;
+
+	nodeEl = d3.select(node);
+	nodeText = d3.select(node.parentNode).select("text");
+
+	displayedText = highlightBool ? ("Over: " + nodeData.name) : "";
+	$(".results__info").html(displayedText);
+
+	nodeEl.classed("node--hovered", highlightBool);
+	nodeText.classed("node-title--node-hovered", highlightBool);
+}
+
+function putNodeInForeground(node) {
+	var groupNode;
+
+	groupNode = node.parentNode;
 	groupNode.parentNode.appendChild(groupNode); // This is too put the node on top of the otehr ones so that we can see the label
-	
-	targetNodes = {};
+}
+
+function highlightEdgesAndGetTargetNodes(highlightBool, nodeData) {
+	var targetNodes = {};
 
 	d3.selectAll(".link")
 	 	.filter(function(d) {
@@ -105,13 +145,17 @@ function mouseoverNode(nodeData) {
 	 	.each(function(d) {
 	 		var targetNodeKey;
 
-	 		d3.select(this).classed("link--node-hovered", true);
+	 		d3.select(this).classed("link--node-hovered", highlightBool);
 
 	 		// This is to highlight the targeted nodes
 	 		targetNodeKey = d.source.index === nodeData.index ? d.target.index : d.source.index;
 	 		targetNodes[targetNodeKey] = true;
 	 	});
 
+	return targetNodes;
+}
+
+function highlighTargetNodes(highlightBool, targetNodes) {
 	d3.selectAll(".node")
 		.filter(function(d) {
 			if (targetNodes[d.index]) {
@@ -127,52 +171,8 @@ function mouseoverNode(nodeData) {
 	 		targetNode = d3.select(this);
 	 		nodeText = d3.select(this.parentNode).select("text");
 			
-	 		d3.select(this).classed("node--highlight-edge-target", true);
-	 		nodeText.classed("node-title--highlight-edge-target", true);
-	 	});
-
-}
-
-function mouseoutNode(nodeData) {
-	var node, nodeText, links, targetNodes;
-
-	node = d3.select(this);
-	nodeText = d3.select(this.parentNode).select("text");
-
-	node.classed("node--hovered", false);
-	nodeText.classed("node-title--node-hovered", false);
-
-	targetNodes = {};
-
-	d3.selectAll(".link")
-	 	.filter(function(d) {
-	  		return ((nodeData.index == d.source.index) || (nodeData.index == d.target.index));
-	 	})
-	 	.each(function(d) {
-	 		d3.select(this).classed("link--node-hovered", false);
-
-	 		// This is to highlight the targeted nodes
-	 		targetNodeKey = d.source.index === nodeData.index ? d.target.index : d.source.index;
-	 		targetNodes[targetNodeKey] = true;
-	 	});
-
-	d3.selectAll(".node")
-		.filter(function(d) {
-			if (targetNodes[d.index]) {
-				return true;
-			}
-			else {
-				return false;
-			}
-	 	})
-	 	.each(function(d) {
-	 		var targetNode, nodeText;
-
-	 		targetNode = d3.select(this);
-	 		nodeText = d3.select(this.parentNode).select("text");
-
-	 		d3.select(this).classed("node--highlight-edge-target", false);
-	 		nodeText.classed("node-title--highlight-edge-target", false);
+	 		d3.select(this).classed("node--highlight-edge-target", highlightBool);
+	 		nodeText.classed("node-title--highlight-edge-target", highlightBool);
 	 	});
 }
 
