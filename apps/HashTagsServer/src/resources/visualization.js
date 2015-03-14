624d4859bae2be883a9e7377963cdc3cdfb44a5d
zantif <dye182@gmail.com>
2015-03-14 13:41:39 -0700

    Fix .classed
    Needs to still determine class w/ func. dependency

--- apps/HashTagsServer/src/resources/visualization.js
+++ apps/HashTagsServer/src/resources/visualization.js
@@ -71,12 +71,8 @@ function createNodes(JSONdata, graphFrame, forceGraph) {
 
 	// Append the circles
 	nodesGroup.append("circle")
-		.attr("class", function(d) { 
-			if(d.hashtagOrMention == 1) return "node--mention";
-			else return "node";
-		})
-		// mySelection.classed( name, func )
-		.attr("class", function(d) { if(d.hashtagOrMention == 1) {return 'node--mention:';} else {return 'node';}});
+		.classed("node", true)
+		// .attr("class", function(d) { if(d.hashtagOrMention == 1) {return 'node--mention:';} else {return 'node';}});
 		.attr("r", function(d) { return d.radius; }) // We could make a function to get the radius bigger: attr(r, function(d) { return d.value; })
 		.on("mouseover", mouseoverNode)
 		.on("mouseout", mouseoutNode)
