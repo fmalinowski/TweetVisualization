b7b3036c3519cd560fdd8c971eb8d1a844481ade
zantif <dye182@gmail.com>
2015-03-14 19:02:07 -0700

    Add different color for mention and hashtag
    fix visualization .js

--- apps/HashTagsServer/src/resources/visualization.js
+++ apps/HashTagsServer/src/resources/visualization.js
@@ -71,8 +71,7 @@ function createNodes(JSONdata, graphFrame, forceGraph) {
 
 	// Append the circles
 	nodesGroup.append("circle")
-		.classed("node", true)
-		// .attr("class", function(d) { if(d.hashtagOrMention == 1) {return 'node--mention:';} else {return 'node';}});
+		.attr("class", function(d) { if(d.type == 1) {return 'node--mention';} else {return 'node';}})
 		.attr("r", function(d) { return d.radius; }) // We could make a function to get the radius bigger: attr(r, function(d) { return d.value; })
 		.on("mouseover", mouseoverNode)
 		.on("mouseout", mouseoutNode)
