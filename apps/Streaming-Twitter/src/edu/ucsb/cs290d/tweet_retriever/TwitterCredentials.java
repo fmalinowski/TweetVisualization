42f04a06b87ef1501cd644a82917c2d2be58541f
fmalinowski <francois@francois-malinowski.fr>
2015-03-17 18:41:53 -0700

    fm - Clean the unused folders

--- apps/Streaming-Twitter/src/edu/ucsb/cs290d/tweet_retriever/TwitterCredentials.java
+++
@@ -1,8 +0,0 @@
-package edu.ucsb.cs290d.tweet_retriever;
-
-public class TwitterCredentials {
-	public static final String CONSUMER_KEY = "";
-	public static final String CONSUMER_SECRET = "";
-	public static final String ACCESS_TOKEN = "";
-	public static final String ACCESS_TOKEN_SECRET = "";
-}
